e68332c74860e74eab20e21814a555d3af968d9b
Dawntraoz <albasilventefu@gmail.com>
2025-11-20 17:28:32 +0100

    feat: add listing details page, search with pagination and ListingPreview component to render the listing summary (1st approach)

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,3 +1,50 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const currentPage = ref(Number(route.query.page ?? 1));
+const { data, pending } = await useFetch("/api/listings", {
+  query: { page: currentPage },
+});
+
+watch(currentPage, (newPage) => {
+  navigateTo({ query: { page: newPage } });
+});
+</script>
+
 <template>
-  <p>Search results</p>
+  <h1 class="font-semibold text-xl max-w-4xl mx-auto">Search results</h1>
+  <div v-if="pending">Loading...</div>
+  <div v-else-if="!pending && data" class="max-w-4xl mx-auto">
+    <div class="py-4 grid gap-6">
+      <ListingPreview
+        v-for="(listing, index) in data.listings"
+        :key="listing.Id"
+        :listing="listing"
+        :order="index"
+      />
+    </div>
+    <nav class="flex justify-between">
+      <button
+        :class="$style['btn-prev']"
+        :disabled="currentPage <= 1"
+        @click="currentPage--"
+      >
+        Previous
+      </button>
+      <button
+        :class="$style['btn-next']"
+        :disabled="currentPage >= (data?.pageInfo?.AantalPaginas ?? 1)"
+        @click="currentPage++"
+      >
+        Next
+      </button>
+    </nav>
+  </div>
 </template>
+
+<style module>
+.btn-prev,
+.btn-next {
+  @apply px-4 py-2 text-secondary-70 hover:bg-secondary-10 rounded-md border border-secondary-10 disabled:(text-gray-400 bg-gray-50 border-transparent hover:bg-gray-50);
+}
+</style>
