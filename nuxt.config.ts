ac5eabb39c4f4179d1aab4497c5ab837ff2cd14a
Dawntraoz <albasilventefu@gmail.com>
2025-11-20 17:26:24 +0100

    feat: add NuxtImage module, add custom icons to use in the listing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@unocss/nuxt',
+    '@nuxt/image'
   ],
+  image: {
+    domains: ['cloud.funda.nl'],
+  },
   runtimeConfig: {
     baseURL: process.env.NUXT_BASE_URL || 'https://partnerapi.funda.nl/feeds/Aanbod.svc/json',
     apiToken: process.env.NUXT_API_TOKEN || '',
